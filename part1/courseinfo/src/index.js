eb9c2bdd3931666a2167a071e3834423effd228a
Krister Torikka <krister.torikka@gmail.com>
2020-03-25 23:07:40 +0200

    Exercise 1.8 done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+const Statistics = ({ good, setGood, neutral, setNeutral, bad, setBad }) => {
   const avg = (1 * good + 0 * neutral + -1 * bad) / (good + neutral + bad);
   const positive = good / (good + neutral + bad);
 
   return (
-    <div>
+    <>
       <h1>Give feedback</h1>
       <button onClick={() => setGood(good + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
@@ -22,6 +18,19 @@ const App = () => {
       <p>All: {good + neutral + bad}</p>
       <p>Average: {avg ? avg : 0}</p>
       <p>Positive: {(positive ? positive : 0) * 100} %</p>
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <Statistics good={good} setGood={setGood} neutral={neutral} setNeutral={setNeutral} bad={bad} setBad={setBad} />
     </div>
   );
 };
