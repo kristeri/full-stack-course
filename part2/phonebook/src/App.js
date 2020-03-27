c142697fa0cad0bb20467ca6e903751d19143b5f
Krister Torikka <krister.torikka@gmail.com>
2020-03-27 13:24:29 +0200

    Exercise 2.8 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "040-1234567" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = event => {
     event.preventDefault();
-    const obj = { name: newName };
+    const obj = { name: newName, number: newNumber };
     if (!persons.some(person => person.name === newName)) {
       const copy = [...persons];
       copy.push(obj);
@@ -24,12 +25,19 @@ const App = () => {
           name: <input onChange={event => setNewName(event.target.value)} />
         </div>
         <div>
+          number: <input onChange={event => setNewNumber(event.target.value)} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person => {
-        return <p key={person.name}>{person.name}</p>;
+        return (
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>
+        );
       })}
     </div>
   );
