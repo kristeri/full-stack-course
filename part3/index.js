52f9c9ad8f15961ccd37e2ae5393cf7d774532b8
Krister Torikka <krister.torikka@gmail.com>
2020-03-29 20:45:49 +0300

    Exercise 3.6 done

--- part3/index.js
+++ part3/index.js
@@ -61,13 +61,19 @@ app.post("/api/persons", (request, response) => {
 
   if (!body.name) {
     return response.status(400).json({
-      error: "Name missing"
+      error: "Name missing."
     });
   }
 
   if (!body.number) {
     return response.status(400).json({
-      error: "Number missing"
+      error: "Number missing."
+    });
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: "Name must be unique."
     });
   }
 
