bb763b32fa607b5ebd7670ae9bc37c722aef0cd2
Krister Torikka <krister.torikka@gmail.com>
2020-04-01 22:56:01 +0300

    Exercise 4.14 done

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -19,6 +19,23 @@ blogsRouter.post("/blogs", (request, response) => {
   }
 });
 
+blogsRouter.put("/blogs/:id", (request, response, next) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  };
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON());
+    })
+    .catch(error => next(error));
+});
+
 blogsRouter.delete("/blogs/:id", async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id);
   response.status(204).end();
