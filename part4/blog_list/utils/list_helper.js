483a86a3f485ada8e0cd885f27e9e6954de4f7d3
Krister Torikka <krister.torikka@gmail.com>
2020-04-01 19:25:01 +0300

    Exercise 4.4 done

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = blogs => {
   return 1;
 };
 
+const totalLikes = blogs => {
+  return blogs.reduce((prev, next) => prev + next.likes, 0);
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 };
