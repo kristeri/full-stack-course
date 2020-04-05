6c3ad6a2711b663f29addf06ee14138a6e650623
Krister Torikka <krister.torikka@gmail.com>
2020-04-05 22:37:36 +0300

    Exercise 5.5 and 5.6 done

--- part5/bloglist-frontend-master/src/App.js
+++ part5/bloglist-frontend-master/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import Notification from "./components/Notification";
+import Togglable from "./components/Togglable";
+import BlogForm from "./components/BlogForm";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [newBlog, setNewBlog] = useState({ title: "", author: "", url: "" });
+
   const [errorMessage, setErrorMessage] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -69,62 +71,23 @@ const App = () => {
     </form>
   );
 
-  const addBlog = (event) => {
-    event.preventDefault();
-    const blogObject = { ...newBlog };
+  const blogFormRef = React.createRef();
 
-    blogService
-      .create(blogObject)
-      .then((returnedBlog) => {
-        setBlogs(blogs.concat(returnedBlog));
-        setNewBlog({ title: "", author: "", url: "" });
-      })
-      .then(() => {
-        setErrorMessage(`A new blog ${blogObject.title} by ${blogObject.author} added.`);
-        setTimeout(() => {
-          setErrorMessage(null);
-        }, 5000);
-      });
-  };
-
-  const handleBlogChange = (event, type) => {
-    const newBlogCopy = { ...newBlog };
-    switch (type) {
-      case "title":
-        newBlogCopy.title = event.target.value;
-        setNewBlog(newBlogCopy);
-        break;
-      case "author":
-        newBlogCopy.author = event.target.value;
-        setNewBlog(newBlogCopy);
-        break;
-      case "url":
-        newBlogCopy.url = event.target.value;
-        setNewBlog(newBlogCopy);
-        break;
-      default:
-        return;
-    }
+  const addBlog = (blogObject) => {
+    blogFormRef.current.toggleVisibility();
+    blogService.create(blogObject).then((returnedBlog) => {
+      setBlogs(blogs.concat(returnedBlog));
+      setErrorMessage(`A new blog ${blogObject.title} by ${blogObject.author} added.`);
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    });
   };
 
   const blogForm = () => (
-    <form onSubmit={addBlog}>
-      <div>
-        <span>Title: </span>
-        <input value={newBlog.title} onChange={(event) => handleBlogChange(event, "title")} />
-      </div>
-      <div>
-        <span>Author: </span>
-        <input value={newBlog.author} onChange={(event) => handleBlogChange(event, "author")} />
-      </div>
-      <div>
-        <span>Url: </span>
-        <input value={newBlog.url} onChange={(event) => handleBlogChange(event, "url")} />
-      </div>
-      <div>
-        <button type="submit">Create</button>
-      </div>
-    </form>
+    <Togglable buttonLabel="new note" ref={blogFormRef}>
+      <BlogForm createBlog={addBlog} />
+    </Togglable>
   );
 
   return (
---
+++ part5/bloglist-frontend-master/src/components/BlogForm.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+const BlogForm = ({ createBlog }) => {
+  const [newBlog, setNewBlog] = useState({ title: "", author: "", url: "" });
+
+  const addBlog = (event) => {
+    event.preventDefault();
+    createBlog(newBlog);
+    setNewBlog({ title: "", author: "", url: "" });
+  };
+
+  const handleBlogChange = (event, type) => {
+    const newBlogCopy = { ...newBlog };
+    switch (type) {
+      case "title":
+        newBlogCopy.title = event.target.value;
+        setNewBlog(newBlogCopy);
+        break;
+      case "author":
+        newBlogCopy.author = event.target.value;
+        setNewBlog(newBlogCopy);
+        break;
+      case "url":
+        newBlogCopy.url = event.target.value;
+        setNewBlog(newBlogCopy);
+        break;
+      default:
+        return;
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={addBlog}>
+        <h2>Create a new blog</h2>
+        <div>
+          <span>Title: </span>
+          <input value={newBlog.title} onChange={(event) => handleBlogChange(event, "title")} />
+        </div>
+        <div>
+          <span>Author: </span>
+          <input value={newBlog.author} onChange={(event) => handleBlogChange(event, "author")} />
+        </div>
+        <div>
+          <span>Url: </span>
+          <input value={newBlog.url} onChange={(event) => handleBlogChange(event, "url")} />
+        </div>
+        <div>
+          <button type="submit">Create</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default BlogForm;
