34b8ae5b43f08994db50f3e56f88dd5115c0d616
Krister Torikka <krister.torikka@gmail.com>
2020-05-05 18:05:42 +0300

    Exercise 6.13 and 6.14 done

--- part6/redux-anecdotes/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/redux-anecdotes/src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
+import { useDispatch } from "react-redux";
+import { initializeAnecdotes } from "./reducers/anecdoteReducer";
+import anecdoteService from "./services/anecdotes";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService.getAll().then((anecdotes) => dispatch(initializeAnecdotes(anecdotes)));
+  }, [dispatch]);
+
   return (
     <div>
       <h2>Anecdotes</h2>
--- part6/redux-anecdotes/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -19,14 +19,17 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject);
 
-export const createAnecdote = (content) => {
+export const createAnecdote = (data) => {
   return {
     type: "NEW_ANECDOTE",
-    data: {
-      content,
-      votes: 0,
-      id: getId(),
-    },
+    data,
+  };
+};
+
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: "INIT_ANECDOTES",
+    data: anecdotes,
   };
 };
 
@@ -37,10 +40,12 @@ export const voteAnecdote = (id) => {
   };
 };
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
     case "NEW_ANECDOTE":
       return [...state, action.data];
+    case "INIT_ANECDOTES":
+      return action.data;
     case "VOTE":
       const id = action.data.id;
       const votedAnecdote = state.find((n) => n.id === id);
